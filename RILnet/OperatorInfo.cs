14d9af2140295466f6625cdfdbda3debcf7b34ab
hjgode <hjgode@gmail.com>
2013-08-08 10:42:38 +0200

    added files

---
+++ RILnet/OperatorInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace RilNET
+{
+    [Serializable]
+    public class OperatorInfo
+    {
+        /// <summary>
+        /// Specifies valid parameters. Must be one or a combination of the <see cref="T:RilNET.RIL_PARAM_OI">RIL_PARAM_OI</see> parameter constants.
+        /// </summary>
+        public RIL_PARAM_OI dwParams;
+
+        /// <summary>
+        /// Specifies the index, if applicable.
+        /// </summary>
+        public RIL_PREFOPINDEX dwIndex;
+
+        /// <summary>
+        /// Specifies the registration status, if applicable.
+        /// </summary>
+        public uint dwStatus;
+
+        /// <summary>
+        /// Representations of an operator.
+        /// </summary>
+        public RILOPERATORNAMES ronNames;
+        
+        /// <summary>
+        /// Structure size, in bytes.
+        /// </summary>
+        private uint cbSize;
+
+
+        public OperatorInfo(RILOPERATORINFO oi)
+        {
+            this.dwIndex = oi.dwIndex;
+            this.dwParams = oi.dwParams;
+            this.dwStatus = oi.dwStatus;
+            this.ronNames = oi.ronNames;
+        }
+        public override string ToString()
+        {
+            return this.ronNames.GetLongName();
+        }
+    }
+}
---
+++ RILnet/OperatorNames.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace RilNET
+{
+    [Serializable]
+    public class OperatorNames
+    {
+        /// <summary>
+        /// Specifies valid parameters. Must be one or a combination of the <see cref="T:RilNET.RIL_PARAM_ON">RIL_PARAM_ON</see> parameter constants.
+        /// </summary>
+        public RIL_PARAM_ON dwParams;
+
+        /// <summary>
+        /// Long representation.
+        /// </summary>
+        public string szLongName;
+
+        /// <summary>
+        /// Short representation.
+        /// </summary>
+        public string szShortName;
+
+        /// <summary>
+        /// Numeric representation. Contains the 3-digit country/region code and the 2-digit network code.
+        /// </summary>
+        public string szNumName;
+
+        /// <summary>
+        /// Country/region code. Must be the 2-character International Organization for Standardization (ISO) 3166 country/region representation of the Mobile Country Code (MCC).
+        /// </summary>
+        public string szCountryCode;
+
+
+        public OperatorNames(RILOPERATORNAMES on)
+        {
+            this.dwParams = on.dwParams;
+            this.szCountryCode = on.GetCountryCode();
+            this.szLongName = on.GetLongName();
+            this.szNumName = on.GetNumName();
+            this.szShortName = on.GetShortName();
+        }
+        public override string ToString()
+        {
+            return this.szLongName;
+        }
+    }
+}
